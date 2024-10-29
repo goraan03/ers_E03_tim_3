e380486d5dbb650a87e53c2838b993873343595d
goraan03 <gorangrcic72@gmail.com>
2024-10-29 18:30:36 +0100

    sprint1

--- Common/Klase/Oruzje.cs
+++ Common/Klase/Oruzje.cs
@@ -8,5 +8,19 @@ namespace Common.Klase
 {
     public class Oruzje
     {
+        public string Naziv { get; set; } = string.Empty;
+        public int Cena { get; set; }
+        public int Napad { get; set; }
+        public int Kolicina { get; set; }
+
+        public Oruzje() { }
+
+        public Oruzje(string naziv, int cena, int napad, int kolicina)
+        {
+            Naziv = naziv;
+            Cena = cena;
+            Napad = napad;
+            Kolicina = kolicina;
+        }
     }
 }
--- Common/Klase/Prodavnica.cs
+++ Common/Klase/Prodavnica.cs
@@ -8,6 +8,19 @@ namespace Common.Klase
 {
     public class Prodavnica
     {
+        public int ID { get; set; }
+        public List<Oruzje> Oruzje { get; set; }
+        public List<Napici> Napicis { get; set; }
+        public int Vrednost { get; set; }
+
         public Prodavnica() { }
+
+        public Prodavnica(int iD, List<Oruzje> oruzje, List<Napici> napicis, int vrednost)
+        {
+            ID = iD;
+            Oruzje = oruzje;
+            Napicis = napicis;
+            Vrednost = vrednost;
+        }
     }
 }
