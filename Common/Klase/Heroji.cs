bab591a0d30128cb468c0ff0588d9598af8c1b5a
md1p0 <dipsymiroslav81@gmail.com>
2024-10-29 18:41:28 +0100

    sprint1_end

---
+++ Common/Klase/Heroji.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Klase
+{
+    public class Heroji
+    {
+        public string NazivHeroja { get; set; }
+        public int ZivotniPoeni { get; set; } 
+
+        public int JacinaNapada { get; set; }
+
+        public int StanjeNovcica { get; set; }
+
+        public Heroji(string nazivHeroja, int zivotniPoeni, int jacinaNapada, int stanjeNovcica)
+        {
+            NazivHeroja = nazivHeroja;
+            ZivotniPoeni = zivotniPoeni;
+            JacinaNapada = jacinaNapada;
+            StanjeNovcica = stanjeNovcica;
+        }
+    }
+}
---
+++ Common/Klase/Mapa.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Klase
+{
+    public class Mapa
+    {
+        public string NazivMape { get; set; }
+        public string TipMape { get; set; }
+
+        public int MaxIgraca { get; set; }
+
+        public string PlaviTim { get; set; }
+
+        public string CrveniTim { get; set; }
+
+        public int PomocniEntiteti { get; set; }
+
+        public Mapa(string nazivMape, string tipMape, int maxIgraca, string plaviTim, string crveniTim, int pomocniEntiteti)
+        {
+            NazivMape = nazivMape;
+            TipMape = tipMape;
+            MaxIgraca = maxIgraca;
+            PlaviTim = plaviTim;
+            CrveniTim = crveniTim;
+            PomocniEntiteti = pomocniEntiteti;
+        }
+    }
+}
