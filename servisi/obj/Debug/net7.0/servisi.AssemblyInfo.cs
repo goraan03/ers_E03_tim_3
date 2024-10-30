30621bc23d0330ff4372c33eeaeda7928c9c204e
md1p0 <dipsymiroslav81@gmail.com>
2024-10-30 17:55:31 +0100

    .

--- servisi/obj/Debug/net7.0/servisi.AssemblyInfo.cs
+++ servisi/obj/Debug/net7.0/servisi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Servisi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+869a5a1ec9d047250f8e214329997cf48dadc23b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4db7fa405b6ec3ede6a52c5fac8aa0dea4d90667")]
 [assembly: System.Reflection.AssemblyProductAttribute("Servisi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Servisi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
