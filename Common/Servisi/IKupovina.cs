f2c46adde1ea05c3f9b627a5ce953ee8c3101cfe
md1p0 <dipsymiroslav81@gmail.com>
2024-10-31 18:25:45 +0100

    gotova simulacija

---
+++ Common/Servisi/IKupovina.cs
@@ -0,0 +1,16 @@
+﻿using Common.Modeli;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Servisi
+{
+    public interface IKupovina
+    {
+        void KupovinaProvera(List<Igrac> PlaviTim, List<Igrac> CrveniTim, Prodavnica prod);
+
+        void Kupi(Igrac igr, Prodavnica prod);
+    }
+}
---
+++ servisi/Kupovina/Kupovina.cs
@@ -0,0 +1,65 @@
+﻿using Common.Modeli;
+using Common.Servisi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servisi.Kupovina
+{
+    public class Kupovina : IKupovina
+    {
+        public Kupovina() { }
+
+        public void KupovinaProvera(List<Igrac> PlaviTim, List<Igrac> CrveniTim, Prodavnica prod)
+        {
+            foreach (Igrac i in PlaviTim)
+            {
+                if (i.heroj.StanjeNovcica >= 500)
+                {
+                    Kupi(i, prod);
+                }
+            }
+
+            foreach (Igrac i in CrveniTim)
+            {
+                if (i.heroj.StanjeNovcica >= 500)
+                {
+                    Kupi(i, prod);
+                }
+            }
+        }
+
+        public void Kupi(Igrac igr, Prodavnica prod)
+        {
+            foreach (Oruzje o in prod.Oruzje)
+            {
+                if (o.Kolicina > 0)
+                {
+                    igr.heroj.JacinaNapada += o.Napad;
+                    igr.heroj.StanjeNovcica -= o.Cena;
+                    o.Kolicina--;
+                }
+                else
+                {
+                    Console.WriteLine("Oruzje " + o.Naziv + " vise nije dostupno.");
+                }
+            }
+
+            foreach (Napici n in prod.Napicis)
+            {
+                if (n.Kolicina > 0)
+                {
+                    igr.heroj.JacinaNapada += n.Napad;
+                    igr.heroj.StanjeNovcica -= n.Cena;
+                    n.Kolicina--;
+                }
+                else
+                {
+                    Console.WriteLine("Napitak " + n.Naziv + " vise nije dostupan.");
+                }
+            }
+        }
+    }
+}
