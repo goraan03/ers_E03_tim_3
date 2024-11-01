50c0decc2d9f316cddc86a48206f23b49a0d5f1f
goraan03 <gorangrcic72@gmail.com>
2024-11-01 22:16:52 +0100

    komit

--- servisi/obj/Debug/net7.0/servisi.AssemblyInfo.cs
+++ servisi/obj/Debug/net7.0/servisi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Servisi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8ef4d8b9248a6068eb7ef8035ec8c76ebc1a2f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Servisi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Servisi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
