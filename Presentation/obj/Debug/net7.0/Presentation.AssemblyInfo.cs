2fb547006d80c60ca39c947c401eb271b36d79fc
goraan03 <gorangrcic72@gmail.com>
2024-11-30 14:17:29 +0100

    komit

--- Presentation/obj/Debug/net7.0/Presentation.AssemblyInfo.cs
+++ Presentation/obj/Debug/net7.0/Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c13a7c3f57d7ad54be95acedd9e38515a38a0815")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
